04647edce0f6d6143547fcca9b2d18ffe59bfd4b
shrishtimishra751 <mishrashrishti90@.com>
2019-08-24 19:51:40 +0530

    initial

---
+++ RestTemplate1/src/main/java/com/bl/main/model/Employee.java
@@ -0,0 +1,62 @@
+package com.bl.main.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+
+	public Employee() {
+
+	}
+
+	public Employee(String firstName, String lastName, String emailId) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public long getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+}
---
+++ RestTemplate1/src/main/java/com/bl/main/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.bl.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bl.main.model.Employee;
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
